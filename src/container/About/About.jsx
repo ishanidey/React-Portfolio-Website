4276cfaf53c9b11f4ded27bc004ba97c0dd690f1
ishanidey <20ec01038@iitbbs.ac.in>
2022-07-15 00:30:20 +0530

    few changes

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -8,7 +8,9 @@ const About = () => (
   <>
     <h2 className="head-text">About Me</h2>
     <motion.div
-      transition={{ duration: 1, type: 'tween' }}
+      whileInView={{ x: [100, 0], opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+      className="app__header-info"
     >
 
       <div className="font" id="about">
@@ -16,6 +18,17 @@ const About = () => (
           <p>My name is Ishani. A student of IIT Bhubaneswar, currently pursuing 4th semester in BTECH of electronics and communication department.
           </p>
           <p>Fixing bugs and solving puzzles thrils me the most😀. </p>
+        </div>
+      </div>
+    </motion.div>
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+      className="app__header-info"
+    >
+
+      <div className="font" id="about">
+        <div className="heading">
           <p>My curiosity to know how such magnificent sites are built led me to discover web development😄 and this site is a byproduct of that.</p>
           <p>This site is a single page application built with React.</p>
           <p>A C++ programmer and a wannabe photographer. My target is always to write neat and efficient code.</p>
